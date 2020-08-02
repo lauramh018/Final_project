486c40d03893e5e9e015960b4cd872b65723e2f3
Paolo Vega <pao02esc@gmail.com>
2020-08-02 17:58:24 -0500

    update in file to load data in JS

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
-var svgWidth = 960;
-var svgHeight = 500;
-
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 100
-};
-
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
-
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
-
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
 // Import Data
 d3.json("/Employee_Attrition.json").then((Attrition) =>{
+
+  // Read all rows
+  Attrition.forEach(row => {
+      console.log(row)
+  });
 });
 
 
