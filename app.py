3d06cf3dc978065d2888766d91693583d8df47d0
Paolo Vega <pao02esc@gmail.com>
2020-08-06 19:55:47 -0500

    pv update

--- app.py
+++ app.py
@@ -70,6 +70,89 @@ def age_turnover():
 
     return jsonify({"employees": {"Yes": yes_att.to_dict(),"No":no_att.to_dict()}})
 
+# Calculate numbers and percetages as json
+@app.route("/api/overview/data", methods=["GET", 'POST'])
+def overviewData():
+    # Loading the data
+    raw_data = pd.read_csv('Employee-Attrition.csv')
+    percentages = raw_data["Attrition"].value_counts(normalize=True)
+    numbers = raw_data["Attrition"].value_counts()
+    numbers = {
+        "noPercentage" : round(float(percentages[0]*100),2),
+        "noNumber" : int(numbers[0]),
+        "yesPercentage" : round(float(percentages[1]*100),2),
+        "yesNumber" : int(numbers[1])
+    }
+    return jsonify({"employess": numbers})
+
+# Calculate numbers and percetages as json
+@app.route("/api/turnover/worklifebalance", methods=["GET", 'POST'])
+def turnoverWorkLifeBalance():
+    # Loading the data
+    employees = pd.read_csv('Employee-Attrition.csv')
+    df = employees.loc[employees["Attrition"] == "Yes",["Age","WorkLifeBalance",]]
+    df["Birthdate"] = 2017 - df["Age"]
+    bins = [0,1964,1979,1995,2021]
+    labels = ["Baby Boomers","Gen X","Milennials","Gen Z"]
+    df["Generation"] = pd.cut(df["Birthdate"],bins=bins, labels=labels)
+    df["Total"] = df["Age"].count()
+    df.head()
+
+    dff = []
+    for element in df["Generation"].unique():
+        print(element)
+        temp = df.loc[df["Generation"] == element,:].groupby(["WorkLifeBalance"]).agg({"Age":"count"})
+        temp["Generation"] = element
+        temp["Total"] = round(temp["Age"] / temp["Age"].sum()*100,2)
+        dff.append(temp.to_dict())
+
+    return jsonify({"generations": dff})
+
+# Calculate numbers and percetages as json
+@app.route("/api/turnover/jobsatisfaction", methods=["GET", 'POST'])
+def turnoverJobSatisfaction():
+    # Loading the data
+    employees = pd.read_csv('Employee-Attrition.csv')
+    df = employees.loc[employees["Attrition"] == "Yes",["Age","JobSatisfaction",]]
+    df["Birthdate"] = 2017 - df["Age"]
+    bins = [0,1964,1979,1995,2021]
+    labels = ["Baby Boomers","Gen X","Milennials","Gen Z"]
+    df["Generation"] = pd.cut(df["Birthdate"],bins=bins, labels=labels)
+    df["Total"] = df["Age"].count()
+
+    dff = []
+    for element in df["Generation"].unique():
+        print(element)
+        temp = df.loc[df["Generation"] == element,:].groupby(["JobSatisfaction"]).agg({"Age":"count"})
+        temp["Generation"] = element
+        temp["Total"] = round(temp["Age"] / temp["Age"].sum()*100,2)
+        dff.append(temp.to_dict())
+        
+    return jsonify({"generations": dff})
+
+
+# Calculate numbers and percetages as json
+@app.route("/api/turnover/environment", methods=["GET", 'POST'])
+def turnoverEnvSatisfaction():
+    # Loading the data
+    employees = pd.read_csv('Employee-Attrition.csv')
+    df = employees.loc[employees["Attrition"] == "Yes",["Age","EnvironmentSatisfaction",]]
+    df["Birthdate"] = 2017 - df["Age"]
+    bins = [0,1964,1979,1995,2021]
+    labels = ["Baby Boomers","Gen X","Milennials","Gen Z"]
+    df["Generation"] = pd.cut(df["Birthdate"],bins=bins, labels=labels)
+    df["Total"] = df["Age"].count()
+
+    dff = []
+    for element in df["Generation"].unique():
+        print(element)
+        temp = df.loc[df["Generation"] == element,:].groupby(["EnvironmentSatisfaction"]).agg({"Age":"count"})
+        temp["Generation"] = element
+        temp["Total"] = round(temp["Age"] / temp["Age"].sum()*100,2)
+        dff.append(temp.to_dict())
+        
+    return jsonify({"generations": dff})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
