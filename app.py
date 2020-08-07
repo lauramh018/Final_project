545de273695efeb7452308a00d424a6c6b732abf
Paolo Vega <pao02esc@gmail.com>
2020-08-06 19:48:32 -0500

    pv first graphs

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from flask import render_template
 from flask import redirect
 from flask import jsonify
 from flask import request
+import numpy as np 
+import pandas as pd
+import math
 import json
 
 
@@ -33,6 +36,134 @@ def attrition_info():
         data = json.load(f)
     
     return jsonify(data)
+
+# Calculate numbers and percetages as json
+@app.route("/api/overview/data", methods=["GET", 'POST'])
+def overviewData():
+    # Loading the data
+    raw_data = pd.read_csv('Employee-Attrition.csv')
+    percentages = raw_data["Attrition"].value_counts(normalize=True)
+    numbers = raw_data["Attrition"].value_counts()
+    numbers = {
+        "noPercentage" : round(float(percentages[0]*100),2),
+        "noNumber" : int(numbers[0]),
+        "yesPercentage" : round(float(percentages[1]*100),2),
+        "yesNumber" : int(numbers[1])
+    }
+    return jsonify({"employess": numbers})
+
+
+# Calculate numbers and percetages as json
+@app.route("/api/turnover/generation", methods=["GET", 'POST'])
+def turnoverGeneration():
+    # Loading the data
+    employees = pd.read_csv('Employee-Attrition.csv')
+    df = employees.loc[:,["Age","Attrition"]]
+    df["Birthdate"] = 2017 - df["Age"]
+    bins = [0,1964,1979,1995,2021]
+    labels = ["Baby Boomers","Gen X","Milennials","Gen Z"]
+    df["Generation"] = pd.cut(df["Birthdate"],bins=bins, labels=labels)
+    df["Total"] = df["Age"].count()
+    df["TotalTurnover"] = 0
+    tot = df.groupby("Attrition")["Attrition"].count()
+    df.loc[df["Attrition"] == "Yes", 'TotalTurnover'] = tot[1]
+    df.loc[df["Attrition"] == "No", 'TotalTurnover'] = tot[0]
+    df["TotalTurnover"].value_counts()
+    df = df.groupby(["Attrition","Generation"]).agg({"Age":"count","TotalTurnover":"max"})
+    df["Per"] = round((df["Age"] / df["TotalTurnover"])*100,2)
+    df.reset_index(drop=False, inplace=True)
+    return jsonify({"employees": df.to_dict()})
+
+
+# Calculate numbers and percetages as json
+@app.route("/api/turnover/worklifebalance", methods=["GET", 'POST'])
+def turnoverWorkLifeBalance():
+    # Loading the data
+    employees = pd.read_csv('Employee-Attrition.csv')
+    df = employees.loc[employees["Attrition"] == "Yes",["Age","WorkLifeBalance",]]
+    df["Birthdate"] = 2017 - df["Age"]
+    bins = [0,1964,1979,1995,2021]
+    labels = ["Baby Boomers","Gen X","Milennials","Gen Z"]
+    df["Generation"] = pd.cut(df["Birthdate"],bins=bins, labels=labels)
+    df["Total"] = df["Age"].count()
+    df.head()
+
+    dff = []
+    for element in df["Generation"].unique():
+        print(element)
+        temp = df.loc[df["Generation"] == element,:].groupby(["WorkLifeBalance"]).agg({"Age":"count"})
+        temp["Generation"] = element
+        temp["Total"] = round(temp["Age"] / temp["Age"].sum()*100,2)
+        dff.append(temp.to_dict())
+
+    return jsonify({"generations": dff})
+
+# Calculate numbers and percetages as json
+@app.route("/api/turnover/jobsatisfaction", methods=["GET", 'POST'])
+def turnoverJobSatisfaction():
+    # Loading the data
+    employees = pd.read_csv('Employee-Attrition.csv')
+    df = employees.loc[employees["Attrition"] == "Yes",["Age","JobSatisfaction",]]
+    df["Birthdate"] = 2017 - df["Age"]
+    bins = [0,1964,1979,1995,2021]
+    labels = ["Baby Boomers","Gen X","Milennials","Gen Z"]
+    df["Generation"] = pd.cut(df["Birthdate"],bins=bins, labels=labels)
+    df["Total"] = df["Age"].count()
+
+    dff = []
+    for element in df["Generation"].unique():
+        print(element)
+        temp = df.loc[df["Generation"] == element,:].groupby(["JobSatisfaction"]).agg({"Age":"count"})
+        temp["Generation"] = element
+        temp["Total"] = round(temp["Age"] / temp["Age"].sum()*100,2)
+        dff.append(temp.to_dict())
+        
+    return jsonify({"generations": dff})
+
+
+# Calculate numbers and percetages as json
+@app.route("/api/turnover/environment", methods=["GET", 'POST'])
+def turnoverEnvSatisfaction():
+    # Loading the data
+    employees = pd.read_csv('Employee-Attrition.csv')
+    df = employees.loc[employees["Attrition"] == "Yes",["Age","EnvironmentSatisfaction",]]
+    df["Birthdate"] = 2017 - df["Age"]
+    bins = [0,1964,1979,1995,2021]
+    labels = ["Baby Boomers","Gen X","Milennials","Gen Z"]
+    df["Generation"] = pd.cut(df["Birthdate"],bins=bins, labels=labels)
+    df["Total"] = df["Age"].count()
+
+    dff = []
+    for element in df["Generation"].unique():
+        print(element)
+        temp = df.loc[df["Generation"] == element,:].groupby(["EnvironmentSatisfaction"]).agg({"Age":"count"})
+        temp["Generation"] = element
+        temp["Total"] = round(temp["Age"] / temp["Age"].sum()*100,2)
+        dff.append(temp.to_dict())
+        
+    return jsonify({"generations": dff})
+
+# Calculate numbers and percetages as json
+@app.route("/api/turnover/distance", methods=["GET", 'POST'])
+def turnoverDistance():
+    # Loading the data
+    employees = pd.read_csv('Employee-Attrition.csv')
+    df = employees.loc[:,["Age","Attrition","DistanceFromHome"]]
+    df["Birthdate"] = 2017 - df["Age"]
+    bins = [0,1964,1979,1995,2021]
+    labels = ["Baby Boomers","Gen X","Milennials","Gen Z"]
+    df["Generation"] = pd.cut(df["Birthdate"],bins=bins, labels=labels)
+    df["Total"] = df["Age"].count()
+
+    
+    dff = []
+    for element in df["Generation"].unique():
+        temp = df.loc[df["Generation"] == element,:].groupby(["Attrition"]).agg({"DistanceFromHome":"mean"})
+        temp["Generation"] = element
+        dff.append(temp.to_dict())
+        
+    return jsonify({"generations": dff})
+
 
 
 if __name__ == "__main__":
---
+++ static/js/bars.js
@@ -0,0 +1,359 @@
+
+
+d3.json("/api/turnover/generation").then((incomingData) =>{
+    
+    var employees = incomingData.employees;
+    var attrition = [];
+    var values = [];
+    var generation = [];
+
+
+    Object.keys(employees["Attrition"]).forEach(function(key){
+       attrition.push(employees["Attrition"][key])
+    });
+
+    Object.keys(employees["Per"]).forEach(function(key){
+      values.push(employees["Per"][key])
+   });
+
+   Object.keys(employees["Generation"]).forEach(function(key){
+    generation.push(employees["Generation"][key])
+   });
+
+    
+    var uniqueNames = ["Gen Z","Milennials","Gen X","Baby Boomers"];
+    var x = ["Yes","No"];
+    
+    var traces = [];
+    var colors = ["8EAF9C","C9BB9A","988A84","7D6A81"]
+    
+    for (i = 0; i < uniqueNames.length; i++){
+      var y = [values[i],values[i+4]];
+      var trace = {
+        x: x,
+        y: y,
+        text: y.map(String),
+        textposition: 'auto',
+        name: uniqueNames[i],
+        type: 'bar',
+        mode: 'markers',
+        marker: {
+          color: colors[i],
+            size: 10
+        }
+      };
+      traces.push(trace)
+    }
+      
+    var data = traces;
+      
+    var layout = {
+      barmode: 'stack',
+      title: 'Attrition by Generation',
+      font:{
+        family: 'Raleway, sans-serif'
+      },
+      showlegend: true,
+      xaxis:{title:"Attrition"},
+      yaxis:{title:"% Employees"}
+      };
+      
+    Plotly.newPlot('myDiv', data, layout);
+
+});
+
+d3.json("/api/turnover/worklifebalance").then((incomingData) =>{
+    
+  var generations = incomingData.generations;
+  
+  var values = [];
+  var generation = [];
+  var employees = [];
+
+  var i = 0;
+  generations.forEach(generation => {
+
+    var temp = [];
+    Object.keys(generation["Total"]).forEach(function(key){
+        temp.push(generation["Total"][key]);
+    });
+    values.push(temp)
+    
+  
+    var tempEmp = [];
+    Object.keys(generation["Age"]).forEach(function(key){
+      tempEmp.push(generation["Age"][key]);
+    });
+    employees.push(tempEmp)
+
+    i++
+
+  });
+  console.log(values)
+  console.log(employees)
+  
+  var legends = ["Bad","Good","Better","Best"];
+  var x = ["Gen X","Milennials","Gen Z","Baby Boomers"];
+  //var x = ["Gen Z","Milennials","Gen X","Baby Boomers"];
+  
+  var traces = [];
+  var colors = ["A1B5BD","ECC05F","E4693E","112D4B"]
+  
+  for (i = 0; i < legends.length; i++){
+    var y = [values[0][i],values[1][i],values[2][i],values[3][i]];
+    var text = [employees[0][i],employees[1][i],employees[2][i],employees[3][i]];
+    var trace = {
+      x: x,
+      y: y,
+      text: y.map(String),
+      textposition: 'auto',
+      name: legends[i],
+      type: 'bar',
+      mode: 'markers',
+      marker: {
+        color: colors[i],
+          size: 10
+      }
+    };
+    traces.push(trace)
+  }
+    
+  var data = traces;
+    
+  var layout = {
+    barmode: 'stack',
+    title: 'Work-Life Balance by Generation for the people who left',
+    font:{
+      family: 'Raleway, sans-serif'
+    },
+    showlegend: true,
+    xaxis:{title:"Generation"},
+    yaxis:{title:"% Employees"}
+    };
+    
+  Plotly.newPlot('myDiv2', data, layout);
+  
+})
+
+
+d3.json("/api/turnover/jobsatisfaction").then((incomingData) =>{
+    
+  var generations = incomingData.generations;
+  
+  var values = [];
+  var generation = [];
+  var employees = [];
+
+  var i = 0;
+  generations.forEach(generation => {
+
+    var temp = [];
+    Object.keys(generation["Total"]).forEach(function(key){
+        temp.push(generation["Total"][key]);
+    });
+    values.push(temp)
+    
+  
+    var tempEmp = [];
+    Object.keys(generation["Age"]).forEach(function(key){
+      tempEmp.push(generation["Age"][key]);
+    });
+    employees.push(tempEmp)
+
+    i++
+
+  });
+  console.log(values)
+  console.log(employees)
+  
+  var legends = ["Low","Medium","High","Very High"];
+  var x = ["Gen X","Milennials","Gen Z","Baby Boomers"];
+  //var x = ["Gen Z","Milennials","Gen X","Baby Boomers"];
+  
+  var traces = [];
+  var colors = ["A1B5BD","ECC05F","E4693E","112D4B"]
+  
+  for (i = 0; i < legends.length; i++){
+    var y = [values[0][i],values[1][i],values[2][i],values[3][i]];
+    var text = [employees[0][i],employees[1][i],employees[2][i],employees[3][i]];
+    var trace = {
+      x: x,
+      y: y,
+      text: y.map(String),
+      textposition: 'auto',
+      name: legends[i],
+      type: 'bar',
+      mode: 'markers',
+      marker: {
+        color: colors[i],
+          size: 10
+      }
+    };
+    traces.push(trace)
+  }
+    
+  var data = traces;
+    
+  var layout = {
+    barmode: 'stack',
+    title: 'Job Satisfaction by Generation for the people who left',
+    font:{
+      family: 'Raleway, sans-serif'
+    },
+    showlegend: true,
+    xaxis:{title:"Generation"},
+    yaxis:{title:"% Employees"}
+    };
+    
+  Plotly.newPlot('myDiv3', data, layout);
+  
+})
+
+
+d3.json("/api/turnover/environment").then((incomingData) =>{
+    
+  var generations = incomingData.generations;
+  
+  var values = [];
+  var generation = [];
+  var employees = [];
+
+  var i = 0;
+  generations.forEach(generation => {
+
+    var temp = [];
+    Object.keys(generation["Total"]).forEach(function(key){
+        temp.push(generation["Total"][key]);
+    });
+    values.push(temp)
+    
+  
+    var tempEmp = [];
+    Object.keys(generation["Age"]).forEach(function(key){
+      tempEmp.push(generation["Age"][key]);
+    });
+    employees.push(tempEmp)
+
+    i++
+
+  });
+  console.log(values)
+  console.log(employees)
+  
+  var legends = ["Low","Medium","High","Very High"];
+  var x = ["Gen X","Milennials","Gen Z","Baby Boomers"];
+  //var x = ["Gen Z","Milennials","Gen X","Baby Boomers"];
+  
+  var traces = [];
+  var colors = ["A1B5BD","ECC05F","E4693E","112D4B"]
+  
+  for (i = 0; i < legends.length; i++){
+    var y = [values[0][i],values[1][i],values[2][i],values[3][i]];
+    var text = [employees[0][i],employees[1][i],employees[2][i],employees[3][i]];
+    var trace = {
+      x: x,
+      y: y,
+      text: y.map(String),
+      textposition: 'auto',
+      name: legends[i],
+      type: 'bar',
+      mode: 'markers',
+      marker: {
+        color: colors[i],
+          size: 10
+      }
+    };
+    traces.push(trace)
+  }
+    
+  var data = traces;
+    
+  var layout = {
+    barmode: 'stack',
+    title: 'Environment Satisfaction by Generation for the people who left',
+    font:{
+      family: 'Raleway, sans-serif'
+    },
+    showlegend: true,
+    xaxis:{title:"Generation"},
+    yaxis:{title:"% Employees"}
+    };
+    
+  Plotly.newPlot('myDiv4', data, layout);
+  
+})
+
+
+
+d3.json("/api/turnover/distance").then((incomingData) =>{
+    
+  var generations = incomingData.generations;
+  
+  var values = [];
+  var generation = [];
+  var employees = [];
+
+  var i = 0;
+  generations.forEach(generation => {
+
+    var temp = [];
+    Object.keys(generation["DistanceFromHome"]).forEach(function(key){
+        temp.push(generation["DistanceFromHome"][key]);
+    });
+    values.push(temp)
+    
+  
+    var tempEmp = [];
+    Object.keys(generation["Generation"]).forEach(function(key){
+      tempEmp.push(generation["Generation"][key]);
+    });
+    employees.push(tempEmp)
+
+    i++
+
+  });
+  console.log(values)
+  console.log(employees)
+  
+  var legends = ["No","Yes"];
+  var x = ["Gen X","Milennials","Gen Z","Baby Boomers"];
+  //var x = ["Gen Z","Milennials","Gen X","Baby Boomers"];
+  
+  var traces = [];
+  var colors = ["9F9E9E","41a446","E4693E","112D4B"]
+  
+  for (i = 0; i < legends.length; i++){
+    var y = [values[0][i],values[1][i],values[2][i],values[3][i]];
+    var text = [employees[0][i],employees[1][i],employees[2][i],employees[3][i]];
+    var trace = {
+      x: x,
+      y: y,
+      text: y.map(String),
+      textposition: 'auto',
+      name: legends[i],
+      type: 'bar',
+      mode: 'markers',
+      marker: {
+        color: colors[i],
+          size: 10
+      }
+    };
+    traces.push(trace)
+  }
+    
+  var data = traces;
+    
+  var layout = {
+    barmode: 'stack',
+    title: 'Distance by Generation for the people who left',
+    font:{
+      family: 'Raleway, sans-serif'
+    },
+    showlegend: true,
+    xaxis:{title:"Generation"},
+    yaxis:{title:"Avg Kilometers"}
+    };
+    
+  Plotly.newPlot('myDiv5', data, layout);
+  
+})
