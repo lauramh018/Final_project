c561e5aca0657e6bfa30a11ea8fe4ec82d154986
Paolo Vega <pao02esc@gmail.com>
2020-08-07 22:09:55 -0500

    adjustments prediction

--- static/js/app.js
+++ static/js/app.js
@@ -1,68 +1,3 @@
-// Import Data
-d3.json("/api/turnover/ages").then((Attrition) =>{
-  var employees_Yes = Attrition.employees.Yes;
-  var employees_No = Attrition.employees.No;
-  var age_Y = [];
-  var age_N = [];
-  var count_Y = [];
-  var count_N = [];
-  // console.log(employees_Yes.Age);
-  // console.log(employees_No);
-
-  // // // Read all rows
-  Object.keys(employees_Yes["Age"]).forEach(function(key){
-    age_Y.push(employees_Yes["Age"][key])
-  });
-  Object.keys(employees_No["Age"]).forEach(function(key){
-    age_N.push(employees_No["Age"][key])
-  });
-  Object.keys(employees_Yes["Count"]).forEach(function(key){
-    count_Y.push(employees_Yes["Count"][key])
-  });
-  Object.keys(employees_No["Count"]).forEach(function(key){
-    count_N.push(employees_No["Count"][key])
-    
-  });
-  console.log(age_Y)
-  
-  var trace1 = { 
-    x:age_N ,
-    y: count_N,
-    fill: 'tonexty', 
-    type: 'scatter', 
-    mode: 'none',
-    name: "No",
-   };
- 
-  var trace2 = { 
-    x:age_Y,
-    y: count_Y, 
-    fill: 'tozeroy',
-    type: 'scatter', 
-    mode: 'none',
-    name: "Yes" };
-
-  
-
-  var layout = { 
-    title: 'Attrition by age ',
-    font:{
-      family: 'Raleway, sans-serif'
-    },
-    /*width: 500,
-    height: 300,*/
-    showlegend: true,
-    xaxis:{title:"Ages"},
-    yaxis:{title:"Attrition"}
-    }; 
-  
-
-  var data = [trace1, trace2];
-
-  Plotly.newPlot('stacked', data, layout,{displayModeBar: false}, {responsive: true});
-});
-
-
 // select the buttons
 var button1 = d3.select("#case1");
 button1.on("click",case1);
@@ -73,8 +8,29 @@ button3.on("click",case3);
 var button4 = d3.select("#case4");
 button4.on("click",case4);
 
+// Load predicted values
+d3.json(`/api/turnover/prediction`).then((incomingData) =>{
+  var predictions = incomingData.employees;
+  //Load results
+  d3.select("#result1").append("h4").text(predictions["P1"][0]);
+  d3.select("#result2").append("h4").text(predictions["P2"][0]);
+  d3.select("#result3").append("h4").text(predictions["P3"][0]);
+  d3.select("#result4").append("h4").text(predictions["P4"][0]);
+
+})
+
+$(document).ready(function(){
+  $("#result1").hide();
+  $("#result2").hide();
+  $("#result3").hide();
+  $("#result4").hide();
+  
+});
 
 function case1(){
+  $("#result1").show();
+
+  /*
   d3.json(`/api/turnover/prediction`).then((incomingData) =>{
     var predictions = incomingData.employees;
     console.log(predictions["P1"][0])
@@ -82,10 +38,13 @@ function case1(){
     var result = output.append("h4");
     result.text(predictions["P1"][0]);
   })
+  */
 
 }
 
 function case2(){
+  $("#result2").show();
+  /*
   d3.json(`/api/turnover/prediction`).then((incomingData) =>{
     var predictions = incomingData.employees;
     console.log(predictions["P2"][0])
@@ -93,9 +52,12 @@ function case2(){
     var result = output.append("h4");
     result.text(predictions["P2"][0]);
   })
+  */
 
 }
 function case3(){
+  $("#result3").show();
+  /*
   d3.json(`/api/turnover/prediction`).then((incomingData) =>{
     var predictions = incomingData.employees;
     console.log(predictions["P3"][0])
@@ -103,9 +65,12 @@ function case3(){
     var result = output.append("h4");
     result.text(predictions["P3"][0]);
   })
+  */
 
 }
 function case4(){
+  $("#result4").show();
+  /*
   d3.json(`/api/turnover/prediction`).then((incomingData) =>{
     var predictions = incomingData.employees;
     console.log(predictions["P4"][0])
@@ -113,6 +78,7 @@ function case4(){
     var result = output.append("h4");
     result.text(predictions["P4"][0]);
   })
+  */
 
 }
     
--- static/js/bars.js
+++ static/js/bars.js
@@ -491,6 +491,69 @@ d3.json("/api/turnover/distance").then((incomingData) =>{
   
 })
 
+// Import Data
+d3.json("/api/turnover/ages").then((Attrition) =>{
+  var employees_Yes = Attrition.employees.Yes;
+  var employees_No = Attrition.employees.No;
+  var age_Y = [];
+  var age_N = [];
+  var count_Y = [];
+  var count_N = [];
+  // console.log(employees_Yes.Age);
+  // console.log(employees_No);
+
+  // // // Read all rows
+  Object.keys(employees_Yes["Age"]).forEach(function(key){
+    age_Y.push(employees_Yes["Age"][key])
+  });
+  Object.keys(employees_No["Age"]).forEach(function(key){
+    age_N.push(employees_No["Age"][key])
+  });
+  Object.keys(employees_Yes["Count"]).forEach(function(key){
+    count_Y.push(employees_Yes["Count"][key])
+  });
+  Object.keys(employees_No["Count"]).forEach(function(key){
+    count_N.push(employees_No["Count"][key])
+    
+  });
+  console.log(age_Y)
+  
+  var trace1 = { 
+    x:age_N ,
+    y: count_N,
+    fill: 'tonexty', 
+    type: 'scatter', 
+    mode: 'none',
+    name: "No",
+   };
+ 
+  var trace2 = { 
+    x:age_Y,
+    y: count_Y, 
+    fill: 'tozeroy',
+    type: 'scatter', 
+    mode: 'none',
+    name: "Yes" };
+
+  
+
+  var layout = { 
+    title: 'Attrition by age ',
+    font:{
+      family: 'Raleway, sans-serif'
+    },
+    /*width: 500,
+    height: 300,*/
+    showlegend: true,
+    xaxis:{title:"Ages"},
+    yaxis:{title:"Attrition"}
+    }; 
+  
+
+  var data = [trace1, trace2];
+
+  Plotly.newPlot('stacked', data, layout,{displayModeBar: false}, {responsive: true});
+});
 
 
 
